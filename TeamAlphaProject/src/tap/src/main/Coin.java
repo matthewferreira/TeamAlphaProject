12713e494900955c740f69e328b4de6da3b9e67b
Albert <Albert@DESKTOP-J3H6AC6.attlocal.net>
2020-05-03 01:51:22 -0700

    Coin.java now makes use of GameObject

--- TeamAlphaProject/src/tap/src/main/Coin.java
+++ TeamAlphaProject/src/tap/src/main/Coin.java
@@ -4,7 +4,6 @@ import java.awt.Graphics;
 
 
 public class Coin extends GameObject implements Entity {
-	private double x,y;
 	private Textures text;
 	
 	public Coin(double x,double y,Textures text)

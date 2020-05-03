1ca58cf49f6c67fa10fda667bc77d596c8576932
Albert <Albert@DESKTOP-J3H6AC6.attlocal.net>
2020-05-03 02:20:22 -0700

    added getBounds method in GameOBject (returns Rectangle)

--- TeamAlphaProject/src/tap/src/main/GameObject.java
+++ TeamAlphaProject/src/tap/src/main/GameObject.java
@@ -1,10 +1,16 @@
 package tap.src.main;
 
+import java.awt.Rectangle;
+
 public class GameObject {
 	public double x, y;
 	
 	public GameObject(double x, double y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	public Rectangle getBounds(int width, int height) {
+		return new Rectangle((int)x, (int)y, width, height);
 	}
 }

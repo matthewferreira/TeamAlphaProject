5eff617e918ff2bf1e6aa9fbe48a5a8f2d807ac0
RavensSac <ravencunanan@csus.edu>
2020-05-04 17:48:47 -0700

    Fixed Animation Class

--- TeamAlphaProject/src/tap/src/main/Animation.java
+++ TeamAlphaProject/src/tap/src/main/Animation.java
@@ -1,120 +1,75 @@
 package tap.src.main;
-//
+
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-//handles animation
-public class Animation {
 
+public class Animation {
+//Currently only supports 2 frame animations...
 		private int speed;
 		private int frames;
 		private int index = 0;
 		private int count = 0;
-		private int col;//The number of columns of the image to run the animation.
-		private int row;//The number of rows of the image to run the animation
-		private BufferedImage image;//image to get the sprite sheets.
-		private BufferedImage[] images;//array of sprites
-		private BufferedImage currentImage;//current sprite
-		private boolean alive;//find out if animation is finished
-
-		public Animation(BufferedImage image, int frames, int speed, int col, int row) {
-
-			this.image = image;
-
-			this.frames = frames;
+		
+		private BufferedImage img1;
+		private BufferedImage img2;
+		private BufferedImage currentImg;
+		
+		public Animation(int speed,BufferedImage img1,BufferedImage img2) {
 
 			this.speed = speed;
-			this.col = col;
-			this.row = row;
-			images = new BufferedImage[frames];
-			alive = true;
-
-			fillSprites();
-
+			this.img1 = img1;
+			this.img2 = img2;
+			frames=2;
+			
 		}
 		
-		public void fillSprites() // Puts all the sprite sheets in an array.
+		public void runAnimation() // Puts all the sprite sheets in an array.
 		{
-
-			SpriteSheet ss = new SpriteSheet(image);
-
-			int k = 0;
-
-			for (int i = 1; i <= row; i++) {
-
-				for (int j = 1; j <= col; j++, k++) {
-
-					images[k] = ss.grabImage(i, j, (int) image.getWidth() / col, (int) image.getHeight() / row);
-
-				}
-
+			index++;
+			if(index>speed)
+			{
+				index=0;
+				nextFrame();
 			}
 
 		}
 
 		public void nextFrame()//Change the current image with the next sprite.
 		{
-
-			currentImage = images[count];
-
+			if(count==0)
+				currentImg=img1;
+			if(count==1)
+				currentImg=img2;
 			count++;
-
-			if (count >= frames - 1) {  // You can modify frames - 1 to another number if your image does not have sprite sheets in all the image.
-										// If your image has sprite sheets in all the image you can put only frames.
-				alive = false;
-				count = 0;
-
-			}
-
+			if(count>frames)
+			count=0;
+			
 		}
-
-		public boolean isAlive() {
-			return alive;
-		}
-
-		public void setAlive(boolean alive) {
-			this.alive = alive;
-		}
-
-		public void runAnimation()//Uses the method next frames to simulate an animation changing frames in an execution
+		
+		public void drawAnimation(Graphics g,double x,double y,int offset)
 		{
-
-			index++;
-			if (index > speed) {
-
-				index = 0;
-				nextFrame();
-
-			}
-
-		}
-
-		public void drawAnimation(Graphics g, double x, double y, int offset) {
-
-			g.drawImage(currentImage, (int) x - offset, (int) y, null);
-
-		}
-
-		public void setImage(BufferedImage image) {
-			
-		this.image = image;	
-			
+			g.drawImage(currentImg,(int)x-offset,(int)y,null);
 		}
 		
-		public void setFrames(int frames) {
-		
-		this.frames = frames;
+		public void setCount(int count)
+		{
+			this.count=count;
 		}
 		
-		public void setCol(int col) {
-		
-			this.col = col;
-			
+		public int getCount()
+		{
+			return count;
 		}
 		
-		public void setRow(int row) {
-			
-		this.row = row;	
-			
+		public void setSpeed(int speed)
+		{
+			this.speed=speed;
 		}
-			
+		
+		public int getSpeed()
+		{
+			return speed;
+		}
+				
 }
+
